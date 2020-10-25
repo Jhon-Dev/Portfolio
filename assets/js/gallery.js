f2f1bf89846f31942450b06cb2d173bcd13f3823
Jhonatan Nuss <jhonatan.nuss082897@gmail.com>
2020-10-25 01:44:21 -0300

    Adicionado img na pagina sobre mim e reajuste de fontes

---
+++ assets/js/gallery.js
@@ -0,0 +1,113 @@
+var galleryItems = [
+    
+    {
+    id: 0,
+    imgSrc: 'assets/images/dashboard.png',
+    imgAlt: 'Image of the Web App Dashboard  project',
+    heading: 'Web App Dashboard',
+    introduction: 'Web App Dashboard is a web dashboard that uses Chart.js to create line, bar, and pie charts and also  uses local storage to save user changes.',
+    website: 'https://yog9.github.io/Web-App-Dashboard/',
+    gitLink: 'https://github.com/Yog9/Web-App-Dashboard'
+  
+  },
+  {
+    id: 1,
+    imgSrc: 'assets/images/videoplayer.png',
+    imgAlt: 'Image of the Interactive Video Player project',
+    heading:'Interactive Video Player',
+    introduction: 'Interactive Video Player is a  custom video player that uses JavaScript to highlight the transcript below the video as the video progresses.Also when the user clicks any part of the transcript it takes them to the appropriate place in the video.',
+    website: 'https://yog9.github.io/Interactive-video-player/',
+    gitLink: 'https://github.com/Yog9/Interactive-video-player'
+  },
+  {
+    id: 2,
+    imgSrc: 'assets/images/employee.jpg',
+    imgAlt: 'Image of EmployeeDirectory',
+    heading: 'Employee Directory',
+    introduction:'Employee Directory uses Random user generator API to generate random users and their details. Clicking the user would popup a modal with more information.This project included Ajax calls using jQuery and CSS. ',
+    website: 'https://yog9.github.io/Employee-Directory/',
+    gitLink: 'https://github.com/Yog9/Employee-Directory'
+  },
+  {
+    id: 3,
+    imgSrc: 'assets/images/inter.png',
+    imgAlt: 'Image of the Interactive form project',
+    heading: 'Interactive Form',
+    introduction: 'Interactive Form uses JavaScript to enhance the form by adding interactivity like providing helpful error messages when the user tries to submit the form and enters invalid information into the form fields.',
+    website: 'https://yog9.github.io/Interactive-form--website/',
+    gitLink: 'https://github.com/Yog9/Interactive-form--website'
+  },
+  {
+    id: 4,
+    imgSrc: 'assets/images/wheelofsucess.jpg',
+    imgAlt: 'Image of the Wheel of Success project',
+    heading: 'Wheel of Success.',
+    introduction: 'Wheel Of Sucess is a browser version of a word guessing game. It uses Javascript to generate a random phrase that player tries to guess by clicking the letters from an onscreen keyboard.',
+    website: 'https://yog9.github.io/Wheel-Of-Sucess/',
+    gitLink: 'https://github.com/Yog9/Wheel-Of-Sucess'
+  },
+  {
+    id: 5,
+    imgSrc: 'assets/images/photogallery.png',
+    imgAlt: 'Image of the Photo Gallery project',
+    heading: 'Photo Gallery',
+    introduction: 'Photo Gallery is an interactive photo gallery built using JavaScript and a jQuery plugin for a lightbox. It also has a search box funtionality that filters the photos in real-time as the user types, and only display photos that match the search.',
+    website: 'https://yog9.github.io/Photo-Gallery/',
+    gitLink: 'https://github.com/Yog9/Photo-Gallery'
+  },
+  {
+    id: 6,
+    imgSrc: 'assets/images/portfolio.png',
+    imgAlt: 'Image of the Portfolio project',
+    heading:'Portfolio',
+    introduction: 'My full front-end portfolio with web development skills, examples of my projects, contact links, and a bit about me ',
+    website: 'https://yog9.github.io/',
+    gitLink: 'https://github.com/Yog9/Yog9.github.io'
+  },
+  {
+    id: 7,
+    imgSrc: 'assets/images/svg.png',
+    imgAlt: 'Image of the Svg Animation project',
+    heading: 'SVG Animation',
+    introduction: 'SVG Animation uses SVGs to spruce up and add a bit of character to a website.The SVGs were modified by manipulating their styles and animating them.',
+    website: 'https://yog9.github.io/SVG-ANIMATIONS/',
+    gitLink: 'https://github.com/Yog9/SVG-ANIMATIONS'
+  },
+  {
+    id: 8,
+    imgSrc: 'assets/images/responsive.png',
+    imgAlt: 'Image of the Responsive Website project',
+    heading: 'Responsive Layout',
+    introduction:'Responsive Layout is website built using mobile-first principals and uses CSS3 Grid for layouts.The purpose of this project was to understand basics of building responsive webpages using HTML5 and CSS3. ',
+    website: 'https://yog9.github.io/Responsive-Layout/',
+    gitLink: 'https://github.com/Yog9/Responsive-Layout'
+  },
+  {
+    id: 9,
+    imgSrc: 'assets/images/profilepage.png',
+    imgAlt: 'Image of the Profile Page project',
+    heading:'Profile Page',
+    introduction: 'Profile Page is one of the first website that I built using HTML5 and CSS3.It uses Responsive Design principals, CSS transforms and CSS3 pseudo elements',
+    website: 'https://yog9.github.io/My-Personal-Profile-Page',
+    gitLink: 'https://github.com/Yog9/My-Personal-Profile-Page'
+  },
+  {
+    id: 10,
+    imgSrc: 'assets/images/styleguide.png',
+    imgAlt: 'Image of the Web Style Guide project',
+    heading:'Web Style Guide',
+    introduction: 'Web Style Guide is a style guide created using SASS fundamentals like variables, mixins, and functions.',
+    website: 'https://yog9.github.io/Web-Style-Guide/',
+    gitLink: 'https://github.com/Yog9/Web-Style-Guide'
+  },
+  {
+    id: 11,
+    imgSrc: 'assets/images/registration-form.png',
+    imgAlt: 'Image of the Registration Form project',
+    heading: 'Registration Form',
+    introduction: 'Registration Form is a mobile first, responsive form built using a variety of HTML form input types and attributes. The form was enhanced using HTML5 validation properties, CSS3 transitions and pseudo-states.',
+    website: 'https://yog9.github.io/Registration-Form/',
+    gitLink: 'https://github.com/Yog9/Registration-Form'
+  }
+
+  ];
